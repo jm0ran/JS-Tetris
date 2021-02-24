956b80425319c09e58d7655b81bcbc1213a23e39
Joseph Moran <josephdeargmoran@Gmail.com>
2021-02-24 11:40:01 -0500

    Final IST Class 2-24 changes

--- main.js
+++ main.js
@@ -2,31 +2,32 @@ var miliTarget = 500;
 var autoDrop = true;
 
 function setup() { //Setup Function
-	createCanvas(window.innerWidth, window.innerHeight - 20);
+	createCanvas(window.innerWidth, window.innerHeight - 20); //Canvas creation
 };
 
 
-class Piece{
+class Piece{ //Class for piece
 	constructor(pieceType){
-		this.state = 1;
-		this.landed = false;
-		if (pieceType == 1){
-			this.pieceType = 1; //T-Piece
+		this.state = 2; //State of rotation
+		this.landed = false; //Is landed
+		if (pieceType == 1){ //T-Pice
+			this.pieceType = 1; 
 			this.blocks = [[2,2],[2,1],[2,3],[1,2]]; //first block should be point to rotate around
 		};
 	}
 };
-Piece.prototype.draw = function(){
+Piece.prototype.draw = function(){ //draws the piece on screen
 	for (var x = 0; x < this.blocks.length; x++){
 		var location = this.blocks[x];
 		rect(tetrisWindow.xOffset + location[0] * block.length, tetrisWindow.yOffset + location[1] * block.length, tetrisWindow.width / 10, tetrisWindow.height / 24);
 	}
 }
 
-Piece.prototype.move = function(direction) {
+Piece.prototype.move = function(direction) { //Moves the piece
 	var index = null;
 	var modifier = null;
 	switch(direction){
+		//index 0 is x axis 1 is y, modifier is how much to change by
 		case("up"):
 			index = 1;
 			modifier = -1;
@@ -44,20 +45,20 @@ Piece.prototype.move = function(direction) {
 			modifier = 1;
 			break;
 	}
-	if (index != null && modifier != null){
+	if (index != null && modifier != null){ //Makes sure both the index and modifier exist
 		for (var x = 0; x < this.blocks.length; x++){
 			this.blocks[x][index] += modifier;
 		}
 	}
-	piece1.trackFall();				
+	piece1.trackFall(); //Checks for where the piece is each time the move function is run, move method is used by both player and autoDrop		
 }
-Piece.prototype.rotate = function(){
+Piece.prototype.rotate = function(){ //Rotating pieces
 	var modifiers = new Array;
-	if (this.pieceType == 1){//change to switches one day
+	if (this.pieceType == 1){//change to switches one day, seperate one for each piece, math is hard... too hard for me to care
 		switch (this.state){ //This code needs to be simplified and it should be that bad, just need xMod, yMod vars and a for loop after cases
 			//fix side states 
 			case(1):
-				modifiers = [[0,-1],[0,1],[-1, 0]];
+				modifiers = [[0,-1],[0,1],[-1, 0]]; 
 				this.state = 2
 				break;
 			case(2):
@@ -80,14 +81,14 @@ Piece.prototype.rotate = function(){
 	}
 
 }
-Piece.prototype.writeBlocks = function(){
+Piece.prototype.writeBlocks = function(){ //Write blocks to previously dropped blocks array, I want to check for line clears here
 	for (var x = 0; x < this.blocks.length; x++){
 		block.backlog.push(this.blocks[x]);
 	}
 	piece1 = new Piece(1);
 }
 
-Piece.prototype.trackFall = function(){
+Piece.prototype.trackFall = function(){ //Tracks fall and checks for piece placement 
 	if (!this.landed){
 		var sBacklog = JSON.stringify(block.backlog);
 		for (var x = 0; x < this.blocks.length; x++){
